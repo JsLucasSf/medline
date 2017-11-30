6612e05ce20ce17c7790dc1c9eefdc6a4f8b442d
mahtejo <marcela.nogueira@ccc.ufcg.edu.br>
2017-11-29 21:45:01 -0300

    Alterei tanta coisa que nem lembro mais o que eu tava tentando fazer..

--- views/js/script.js
+++ views/js/script.js
@@ -1,30 +1,53 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it RoutingApp
+	var scotchApp = angular.module('medlineApp', ['ngRoute']);
     
         // configure our routes
-        scotchApp.config(function($routeProvider) {
+        scotchApp.config(function($routeProvider, $locationProvider) {
+            console.log("route");
             $routeProvider
+
+                //route for the home page
+                .when('/home', {
+                    templateUrl : 'home.ejs',
+                    controller  : 'mainController'
+                })
     
                 // route for the agenda page
                 .when('/agenda', {
-                    templateUrl : '/home.html#agenda',
+                    templateUrl : 'agenda.html',
                     controller  : 'agendaController'
                 })
     
                 // route for the pacientes page
                 .when('/pacientes', {
-                    templateUrl : '/pacientes.html',
+                    templateUrl : 'pacientes.html',
                     controller  : 'pacientesController'
                 })
     
                 // route for the medicos page
                 .when('/medicos', {
-                    templateUrl : '/medicos.html',
+                    templateUrl : 'medicos.html',
                     controller  : 'medicosController'
-                });
+                })
+
+                // route for the config page
+                .when('/config', {
+                    templateUrl : 'config.html',
+                    controller  : 'configController'
+                })
+                
+                .otherwise({
+                    redirectTo: '/home'
+                  });
         });
 
         // create the controller and inject Angular's $scope
+
+    scotchApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Corpo da página principal aqui ';
+    });
+
 	scotchApp.controller('agendaController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Corpo da página de agenda aqui ';
@@ -37,7 +60,6 @@
 	scotchApp.controller('medicosController', function($scope) {
 		$scope.message = 'Corpo da página de medicos aqui ';
     });
-    
 
 	scotchApp.controller('configController', function($scope) {
 		$scope.message = 'Corpo da página de configurações aqui ';
